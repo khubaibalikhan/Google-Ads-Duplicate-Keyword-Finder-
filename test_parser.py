a4c12cf57f76f38f5df5f6e2e15442ee95d2e695
khubaibalikhan <khubaibalikhan420@gmail.com>
2025-04-25 16:45:08 +0500

    added some changings

--- test_parser.py
+++ test_parser.py
@@ -1,23 +1,23 @@
 from file_parser import parse_file
 
 # CSV
-with open("test_files/sample.csv", "r") as f:
+with open("sample.csv", "r") as f:
     df_csv = parse_file(f, "csv")
     print("CSV:\n", df_csv.head())
 
 # TSV
-with open("test_files/sample.tsv", "r") as f:
+with open("sample.tsv", "r") as f:
     df_tsv = parse_file(f, "tsv")
     print("TSV:\n", df_tsv.head())
 
 # XLSX
-df_xlsx = parse_file("test_files/sample.xlsx", "xlsx")
+df_xlsx = parse_file("sample.xlsx", "xlsx")
 print("XLSX:\n", df_xlsx.head())
 
 # XML
-df_xml = parse_file("test_files/sample.xml", "xml")
+df_xml = parse_file("sample.xml", "xml")
 print("XML:\n", df_xml.head())
 
 # DOCX
-df_docx = parse_file("test_files/sample.docx", "docx")
+df_docx = parse_file("sample.docx", "docx")
 print("DOCX:\n", df_docx.head())
