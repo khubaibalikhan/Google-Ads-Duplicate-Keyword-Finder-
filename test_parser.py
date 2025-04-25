f159f5ecd471c9c4c9b1140c2446f19deceaada9
khubaibalikhan <khubaibalikhan420@gmail.com>
2025-04-25 16:42:40 +0500

    added all format testing options

--- test_parser.py
+++ test_parser.py
@@ -1,7 +1,23 @@
 from file_parser import parse_file
 
-# Test CSV
-with open("sample.csv", "r") as f:
+# CSV
+with open("test_files/sample.csv", "r") as f:
     df_csv = parse_file(f, "csv")
-    print("CSV Output:")
-    print(df_csv.head())
+    print("CSV:\n", df_csv.head())
+
+# TSV
+with open("test_files/sample.tsv", "r") as f:
+    df_tsv = parse_file(f, "tsv")
+    print("TSV:\n", df_tsv.head())
+
+# XLSX
+df_xlsx = parse_file("test_files/sample.xlsx", "xlsx")
+print("XLSX:\n", df_xlsx.head())
+
+# XML
+df_xml = parse_file("test_files/sample.xml", "xml")
+print("XML:\n", df_xml.head())
+
+# DOCX
+df_docx = parse_file("test_files/sample.docx", "docx")
+print("DOCX:\n", df_docx.head())
