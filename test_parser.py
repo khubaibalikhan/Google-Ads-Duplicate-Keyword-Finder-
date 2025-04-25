7d92b30408f4548185112bcea596321f5fe9063d
khubaibalikhan <khubaibalikhan420@gmail.com>
2025-04-25 16:53:34 +0500

    added 4 different kinds of formats

--- test_parser.py
+++ test_parser.py
@@ -5,19 +5,16 @@ with open("sample.csv", "r") as f:
     df_csv = parse_file(f, "csv")
     print("CSV:\n", df_csv.head())
 
-# TSV
-with open("sample.tsv", "r") as f:
-    df_tsv = parse_file(f, "tsv")
-    print("TSV:\n", df_tsv.head())
+# # TSV
+# with open("sample.tsv", "r") as f:
+#     df_tsv = parse_file(f, "tsv")
+#     print("TSV:\n", df_tsv.head())
 
-# XLSX
-df_xlsx = parse_file("sample.xlsx", "xlsx")
-print("XLSX:\n", df_xlsx.head())
+# # XLSX
+# df_xlsx = parse_file("sample.xlsx", "xlsx")
+# print("XLSX:\n", df_xlsx.head())
 
-# XML
-df_xml = parse_file("sample.xml", "xml")
-print("XML:\n", df_xml.head())
+# # XML
+# df_xml = parse_file("sample.xml", "xml")
+# print("XML:\n", df_xml.head())
 
-# DOCX
-df_docx = parse_file("sample.docx", "docx")
-print("DOCX:\n", df_docx.head())
