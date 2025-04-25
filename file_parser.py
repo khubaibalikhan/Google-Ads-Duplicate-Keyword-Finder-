3de9fb8e2774b2ef438ea25c250e77fa12213cbc
khubaibalikhan <khubaibalikhan420@gmail.com>
2025-04-25 15:28:33 +0500

    if in docs, I cant read by pandas, so a manua logic of reading each row is applied

--- file_parser.py
+++ file_parser.py
@@ -4,3 +4,11 @@ from docx import Document
 def parse_file(file, filetype):
     if filetype == 'csv':
         return pd.read_csv(file)
+    elif filetype == 'docx':
+        doc = Document(file)  # Loading the Word file
+        data = []
+        for table in doc.tables:
+            for row in table.rows:
+                data.append([cell.text.strip() for cell in row.cells])
+        return pd.DataFrame(data[1:], columns=data[0])
+
